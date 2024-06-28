e5e6d3469a4ed0eacf05b3039ad4d77b8a6d1318
takuya2546 <takuya.ma@mail.wu.ac.th>
2024-06-28 14:11:02 +0700

    Add files via upload

--- 65123853.dart
+++ 65123853.dart
@@ -1,124 +1,85 @@
 class Person {
-  String _name; 
-
-  void setName(String name) {
-    _name = name;
-  }
-
-  String getName() {
-    return _name;
-  }
-
+  String _name;
   Person(this._name);
+  String get name => _name; 
+  set name(String name) => _name = name;
 }
 
 class Engine {
-  String _model; 
-  int _speed; 
-
-  void setModel(String model) {
-    _model = model;
-  }
-
-  void setSpeed(int speed) {
-    _speed = speed;
-  }
-
-  String getModel() {
-    return _model;
-  }
-
-  int getSpeed() {
-    return _speed;
-  }
-
+  String _model;
+  int _speed;
   Engine(this._model, this._speed);
-
-  void displayEngineInfo() {
+  String get model => _model;
+  int get speed => _speed;
+  set model(String model) => _model = model;
+  set speed(int speed) => _speed = speed;
+  void displayEngineInfo(){
     print('Model: $_model');
     print('Speed: $_speed km/h');
   }
 }
 
 class Car {
-  String _brand; 
-  String _model; 
-  Person _owner; 
-  Engine _engine; 
-
-  void setBrand(String brand) {
-    _brand = brand;
-  }
-
-  void setModel(String model) {
-    _model = model;
-  }
-
-  void setOwner(Person owner) {
-    _owner = owner;
-  }
-
-  void setEngine(Engine engine) {
-    _engine = engine;
-  }
-
-  String getBrand() {
-    return _brand;
-  }
-
-  String getModel() {
-    return _model;
-  }
-
-  Person getOwner() {
-    return _owner;
-  }
-
-  Engine getEngine() {
-    return _engine;
-  }
+  String _brand;
+  String _model;
+  Person _owner;
+  Engine _engine;
 
   Car(this._brand, this._model, this._owner, this._engine);
-
-  void displayCarInfo() {
-    print(' รายละเอียดรถ ');
-    print('Brand: $_brand');
-    print('Model: $_model');
-    print('Owner: ${_owner.getName()}');
-    print('Engine');
+  
+  String get brand => _brand;
+  String get model => _model;
+  Person get owner => _owner;
+  Engine get engine => _engine;
+  
+  set brand(String brand) => _brand = brand;
+  set model(String model) => _model = model;
+  set owner(Person owner) => _owner = owner;
+  set engine(Engine engine) => _engine = engine;
+  
+  void displayCarInfo(){
+    print('Car: $_brand');
     _engine.displayEngineInfo();
+    print('Owner: ${_owner.name}');
   }
 
-  void run() {
-    print('ความเร็ว ${_engine.getSpeed()} km/h');
+  void run(){
+    print('ความเร็วในการขับ ${_engine.speed} km/h');
   }
 }
 
-class Honda extends Car {
-  String _color; 
-  void setColor(String color) {
-    _color = color;
-  }
-  String getColor() {
-    return _color;
-  }
-
- Honda(String brand, String model, Person owner, Engine engine, this._color) : super(brand, model, owner, engine);
-
-  void run() {
-    print('ความเร็วในการขับ 120 km/h');
+class Honda extends Car{
+  String _color;
+  
+  Honda(String brand, String model, Person owner, Engine engine, this._color) : super(brand, model, owner, engine);
+  String get color => _color;
+  set(String color) => _color = color;
+  void run(){
+    var Espeed = 120;
+    print('ความเร็วในการขับ $Espeed km/h');
   }
   
-  void displayCarInfo() {
-    super.displayCarInfo();
+  void displayCarInfo(){
+    print('Car: $_brand');
     print('Color: $_color');
+    _engine.displayEngineInfo();
+    print('Owner: ${_owner.name}');
   }
 }
-
 void main() {
-  Person owner = Person('SOMBAT');
-  Engine engine = Engine('V8', 200);
-  Honda car = Honda('Honda', 'Civic', owner, engine, 'BLACK');
+  var person = Person('Takuya');
+  var engine = Engine('REVUELTO', 350 );
+  var person2 = Person('Sombat');
+  var engine2 = Engine('Civic', 120);
+  
+
+  var car = Car('LAMBORGHIN', '', person, engine);
+  var honda = Honda('Honda', '', person2, engine2, 'BLACK');
+  print('รายละเอียดรถ');
   car.displayCarInfo();
   car.run();
+
+  print('\nรายละเอียดรถ Honda');
+  honda.displayCarInfo();
+  honda.run();
 }
