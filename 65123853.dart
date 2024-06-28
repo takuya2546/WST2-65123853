a664cd54778bef276631e9aeb344528c1f70e32b
takuya2546 <takuya.ma@mail.wu.ac.th>
2024-06-28 14:15:24 +0700

    Add files via upload

--- 65123853.dart
+++ 65123853.dart
@@ -1,7 +1,7 @@
 class Person {
   String _name;
   Person(this._name);
-  String get name => _name; 
+  String get name => _name;
   set name(String name) => _name = name;
 }
 
@@ -13,7 +13,7 @@ class Engine {
   int get speed => _speed;
   set model(String model) => _model = model;
   set speed(int speed) => _speed = speed;
-  void displayEngineInfo(){
+  void displayEngineInfo() {
     print('Model: $_model');
     print('Speed: $_speed km/h');
   }
@@ -26,52 +26,53 @@ class Car {
   Engine _engine;
 
   Car(this._brand, this._model, this._owner, this._engine);
-  
+
   String get brand => _brand;
   String get model => _model;
   Person get owner => _owner;
   Engine get engine => _engine;
-  
+
   set brand(String brand) => _brand = brand;
   set model(String model) => _model = model;
   set owner(Person owner) => _owner = owner;
   set engine(Engine engine) => _engine = engine;
-  
-  void displayCarInfo(){
-    print('Car: $_brand');
+
+  void displayCarInfo() {
+    print('Brand: $_brand');
     _engine.displayEngineInfo();
     print('Owner: ${_owner.name}');
   }
 
-  void run(){
+  void run() {
     print('ความเร็วในการขับ ${_engine.speed} km/h');
   }
 }
 
-class Honda extends Car{
+class Honda extends Car {
   String _color;
-  
-  Honda(String brand, String model, Person owner, Engine engine, this._color) : super(brand, model, owner, engine);
+
+  Honda(String brand, String model, Person owner, Engine engine, this._color)
+      : super(brand, model, owner, engine);
   String get color => _color;
   set(String color) => _color = color;
-  void run(){
+  void run() {
     var Espeed = 120;
     print('ความเร็วในการขับ $Espeed km/h');
   }
-  
-  void displayCarInfo(){
-    print('Car: $_brand');
+
+  void displayCarInfo() {
+    print('Brand: $_brand');
     print('Color: $_color');
     _engine.displayEngineInfo();
     print('Owner: ${_owner.name}');
   }
 }
+
 void main() {
   var person = Person('Takuya');
-  var engine = Engine('REVUELTO', 350 );
+  var engine = Engine('REVUELTO', 350);
   var person2 = Person('Sombat');
   var engine2 = Engine('Civic', 120);
-  
 
   var car = Car('LAMBORGHIN', '', person, engine);
   var honda = Honda('Honda', '', person2, engine2, 'BLACK');
